432b47c84fefd1edd3e8c91311fa38b61dde4455
Michio Oguro <michio_oguro@hotmail.com>
2015-01-14 11:58:53 +0900

    ドキュメントの整理。

--- R/predict.gamm.r
+++ R/predict.gamm.r
@@ -1,5 +1,10 @@
 #'	predict method for gamm object.
 #'
+#'	This is a predict method for \emph{gamm} object.
+#'	This function is an internal function and not intended to be used directly 
+#'	by users. This function internally pass the arguments to the
+#'	\emph{predict.gam} using \emph{gam} object in \emph{gamm} object.
+#'
 #-------------------------------------------------------------------------------
 #	gammオブジェクト用のpredict関数。内部的にpredict.gamを呼び出す。
 #
