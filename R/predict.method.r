1faca36b1bdce1588d5215e84eaa4c7feb8adb0a
Michio Oguro <michio_oguro@hotmail.com>
2015-03-30 00:00:24 +0900

    ドキュメントの修正。

--- R/predict.method.r
+++ R/predict.method.r
@@ -31,14 +31,23 @@ predict.gamm <- function(
 #'
 #'	This is a predict method for \emph{glmmML} object.
 #'	This function is an internal function and not intended to be used directly 
-#'	by users. This function internally pass the arguments to the
-#'	\emph{predict.gam} using \emph{gam} object in \emph{gamm} object.
+#'	by users. Prediction is made using unconditional values and random effect
+#'	is ignored.
 #'
+#'	@param object a glmmML object.
+#'	@param newdata a data.frame containing data used for prediction.
+#'	@param type
+#'		type of prediction. Default is scale of response variable.
+#'		If "link" is specified, prediction is of link scale is calculated.
 #-------------------------------------------------------------------------------
-#	gammオブジェクト用のpredict関数。内部的にpredict.gamを呼び出す。
+#	glmmML用のpredictメソッド。
 #
 #	Args:
-#		predict.gamを見てください。
+#		object: glmmMLオブジェクト。
+#		newdata: 予測に使うデータが入ったデータフレーム。
+#		type:
+#			予測を計算するスケール。デフォルトでは応答変数のスケールで予測を計算
+#			する。"link"を指定すると、リンク関数のスケールで予測を計算する。
 #-------------------------------------------------------------------------------
 predict.glmmML <- function(object, newdata, type = c("response", "link")){
 	type = match.arg(type)
