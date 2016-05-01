89576ae8315a35e77f5796d7238605628ecab223
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 20:48:31 +0900

    テストコードの指標を修正。

--- inst/tests/testthat/test-cv.models-run-glm.r
+++ inst/tests/testthat/test-cv.models-run-glm.r
@@ -5,7 +5,7 @@ test_that("run cv.models with glm (regression, no cluster)", {
 	data(iris)
 	cv <- cv.models(
 		glm, args.model = list(Sepal.Length ~ ., family = gaussian), data = iris,
-		cv.metrics = c("auc", "mse", "rmse", "informedness"), n.cores = 1
+		cv.metrics = c("mse", "rmse", "r.squared"), n.cores = 1
 	)
 	print(cv)
 	bm <- get.best.models(cv)
@@ -21,7 +21,7 @@ test_that("run cv.models with glm (classification, no cluster)", {
 	cv <- cv.models(
 		glm, args.model = list(Species ~ Petal.Length, family = binomial),
 		data = iris,
-		cv.metrics = c("auc", "mse", "rmse", "informedness"), n.cores = 1
+		cv.metrics = c("auc", "informedness", "ppv", "npv"), n.cores = 1
 	)
 	print(cv)
 	bm <- get.best.models(cv)
