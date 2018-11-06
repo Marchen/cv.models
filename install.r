0e4af870bcf3b88bee6a5735eef761726937e63a
Michio Oguro <michio_oguro@hotmail.com>
2018-11-06 17:54:29 +0900

    chore: Add install script. インストールスクリプトの追加。

---
+++ install.r
@@ -0,0 +1,23 @@
+#------------------------------------------------------------------------------
+#	Change working directory to package directory
+#------------------------------------------------------------------------------
+get.this.file.dir <- function() {
+	cmdArgs <- commandArgs(trailingOnly = FALSE)
+	needle <- "--file="
+	match <- grep(needle, cmdArgs)
+	if (length(match) > 0) {
+		# Rscript
+		return(dirname(sub(needle, "", cmdArgs[match])))
+	} else {
+		# 'source'd via R console
+		return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
+	}
+}
+
+old.wd <- setwd(get.this.file.dir())
+
+#------------------------------------------------------------------------------
+#	Install the package before compiling.
+#------------------------------------------------------------------------------
+system("Rscript -e library(devtools);install()")
+
