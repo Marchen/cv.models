84cbab0bbb773de2085704cd5d77433d4f3ee2c0
Michio Oguro <michio_oguro@hotmail.com>
2018-12-03 11:49:37 +0900

    chore: Update build script. ビルドスクリプトの更新。

--- build.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-mkdir build
-cd build
-git clone https://github.com/Marchen/cv.models.git
-cd cv.models
-Rscript build.r
---
+++ build.sh
@@ -0,0 +1,17 @@
+# Clone repository.
+mkdir build
+cd build
+git clone https://github.com/Marchen/cv.models.git
+cd cv.models
+
+# Remove extra files.
+rm install.r
+rm include.r
+rm .travis.yml
+rm build.bat
+
+# Build package.
+R -e 'devtools::document()'
+R -e 'devtools::build(path = "..")'
+
+cd ../..
